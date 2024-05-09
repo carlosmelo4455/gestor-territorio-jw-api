6b947a3d004f2a31afb70ccc89f02b5e118dd764
Carlos <carloseduardo.hrzon@gmail.com>
2024-05-08 21:18:27 -0300

    first commit

---
+++ src/main/java/com/carlos/jw/organizadorterritoriosjw/OrganizadorTerritoriosJwApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.jw.organizadorterritoriosjw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizadorTerritoriosJwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganizadorTerritoriosJwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carlos/jw/organizadorterritoriosjw/OrganizadorTerritoriosJwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.jw.organizadorterritoriosjw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizadorTerritoriosJwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
